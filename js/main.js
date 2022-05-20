a975d973e7f931a8bdd6591011d007962643b640
olic96 <francoolich@gmail.com>
2022-05-20 16:12:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        currentIndex: 0,
+        images: [
+            {
+                src: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                src: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },
+            {
+                src: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                src: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                src: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+    },
+    
+    methods: {
+        changeImageUp() {
+            // this.currentIndex === 0 ? this.currentIndex = this.images.length - 1 : this.currentIndex--
+            if(this.currentIndex === 0) {
+                this.currentIndex = this.images.length - 1;
+            } else {
+                this.currentIndex--;
+            }
+        },
+            // this.currentIndex === this.images.length - 1 ? this.currentIndex = 0 : this.currentIndex++
+        changeImageDown() {
+            if(this.currentIndex === this.images.length - 1) {
+                this.currentIndex = 0;
+            } else {
+                this.currentIndex++;
+            }
+        }
+    }
+});
+
+
